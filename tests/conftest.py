00b07f66f1c98bf06c885cb201f6c36ea03e34a3
anton <anton.dontsovgs@mail.ru>
2023-06-26 20:49:05 +0300

    add simple test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,33 +4,25 @@ from typing import AsyncGenerator
 import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, \
-    async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.pool import NullPool
 
-from currency.main import app
-from currency.database import get_session
+from database import get_async_session
+from database import Base
+from config import settings
+from main import app
 
-class Base(DeclarativeBase):
-    pass
-# Base = declarative_base()
-
-
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test.db"
-engine_test = create_async_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
-# engine_test = create_async_engine(settings.test_asyncpg_url, future=True, echo=True,)
-TestingSessionLocal = async_sessionmaker(bind=engine_test, class_=AsyncSession, expire_on_commit=False)
+# DATABASE
+DATABASE_URL_TEST = settings.test_asyncpg_url
+engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+async_session_maker = async_sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
 Base.metadata.bind = engine_test
 
 async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with TestingSessionLocal() as session:
+    async with async_session_maker() as session:
         yield session
 
-app.dependency_overrides[get_session] = override_get_async_session
-
+app.dependency_overrides[get_async_session] = override_get_async_session
 
 @pytest.fixture(autouse=True, scope='session')
 async def prepare_database():
