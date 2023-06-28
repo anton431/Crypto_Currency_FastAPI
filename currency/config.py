7be528f7e303fe3b72aeaa776dea47d00e91719a
anton <anton.dontsovgs@mail.ru>
2023-06-28 09:46:05 +0300

    docker

--- currency/config.py
+++ currency/config.py
@@ -13,17 +13,17 @@ class Settings(BaseSettings):
         scheme="postgresql+asyncpg",
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
+        host=os.getenv("POSTGRES_SERVER"),
         port=os.getenv("POSTGRES_PORT"),
-        path=f"/{os.getenv('POSTGRES_DB') or ''}",
+        path=f"/{os.getenv('POSTGRES_DB')}",
     )
     test_asyncpg_url: PostgresDsn = PostgresDsn.build(
         scheme="postgresql+asyncpg",
         user=os.getenv("POSTGRES_USER_TEST"),
         password=os.getenv("POSTGRES_PASSWORD_TEST"),
-        host=os.getenv("POSTGRES_HOST_TEST"),
+        host=os.getenv("POSTGRES_SERVER_TEST"),
         port=os.getenv("POSTGRES_PORT_TEST"),
-        path=f"/{os.getenv('POSTGRES_DB_TEST') or ''}",
+        path=f"/{os.getenv('POSTGRES_DB_TEST')}",
     )
     SECRET_KEY = os.getenv("SECRET_KEY")
 
